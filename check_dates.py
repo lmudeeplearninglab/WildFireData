712a25e5832d252378a9ddd9889ae913ab63f0d9
Tinbite Ermias <156370506+tinbite003@users.noreply.github.com>
2026-09-17 12:43:26 -0700

    No longer needed

--- check_dates.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_csv(r"C:\GradResearch\WildFireData\data output\datasets\firms_palisades_bbox_w_m118p900_s_33p960_e_m118p380_n_34p200\firms_palisades.csv")
-print(df.groupby("firms_source")["acq_date"].agg(["min", "max", "count"]))
